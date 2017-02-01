50d9646968453fd0e13b4357133450c63a6a14a8
DWKessler <DarkSabreWilliams@gmail.com>
2017-02-01 18:33:13 -0500

    Moved styles to their own stylesheet

--- index.php
+++ index.php
@@ -1,31 +1,12 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="css/styles.css" />
+
     <title>Oven Status</title>
   </head>
 
   <body>
-
-    <style>
-      #OvenTable {
-        border:1px solid #C0C0C0;
-        border-collapse:collapse;
-        padding:5px;
-        width: 100%;
-        font-size: 2vw;
-        text-align: center;
-        font-weight: bold;
-      }
-      #OvenTable th {
-        border:1px solid #C0C0C0;
-        padding:5px;
-        background:#F0F0F0;
-      }
-      #OvenTable td {
-        border:1px solid #C0C0C0;
-        padding:5px;
-      }
-    </style>
 
     <table id="OvenTable"  border="1" cellpadding="2"></table>
 
